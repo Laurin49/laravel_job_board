9a0559f6959197112958cabdbd134a74b0ef7906
Laurin49 <welter@diwe.info>
2023-07-10 13:27:10 +0200

    dev03 bread_crumbs component

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
+    
     @foreach ($jobs as $key => $job)
         <x-job-card class="mb-4" :job="$job">
             <x-link-button :href="route('jobs.show', $job)">
