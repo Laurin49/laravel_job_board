7cf0a41faba560b35a5563aa61a3e646c1557b26
Laurin49 <welter@diwe.info>
2023-07-15 15:40:04 +0200

    dev05 Refactoring o.k.

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,9 +1,10 @@
 <div class="relative">
-  <input type="text" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
+  <input x-ref="input-{{ $name }}" type="text" placeholder="{{ $placeholder }}"
+    name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
     class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
-  @if ($formId)
+  @if ($formRef)
     <button type="button" class="absolute top-0 right-0 flex items-center h-full pr-2"
-        onclick="document.getElementById('{{ $name }}').value = ''; document.getElementById('{{ $formId }}').submit();">
+      @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();">
 
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
         class="w-4 h-4 text-slate-500">
@@ -12,4 +13,5 @@
 
     </button>
   @endif
+
 </div>
